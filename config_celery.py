f6b11894780d43c69c0d524090aecc369103a718
mielesantos <mielesnts@gmail.com>
2025-01-05 19:30:00 -0300

    feat: remove pdf

--- config_celery.py
+++ config_celery.py
@@ -1,12 +1,22 @@
+import os
+
 from celery import Celery
+from dotenv import load_dotenv
+
+load_dotenv()
+
+if not os.getenv("CELERY_BROKER_URL"):
+    raise ValueError("CELERY_BROKER_URL not found in .env file")
+if not os.getenv("CELERY_RESULT_BACKEND"):
+    raise ValueError("CELERY_RESULT_BACKEND not found in .env file")
 
 app = Celery(
     "pipeline_process_processo",
-    broker="amqp://guest:guest@rabbitmq:5672//",
+    broker=os.getenv("CELERY_BROKER_URL"),
 )
 
 app.conf.update(
-    result_backend="redis://redis:6379/0",
+    result_backend=os.getenv("CELERY_RESULT_BACKEND"),
     task_serializer="json",
     result_serializer="json",
     accept_content=["json"],
